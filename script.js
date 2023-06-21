408fc52d61a1d982a6c43758cf2d06f8123c6f90
ashhna <134618519+ashhna@users.noreply.github.com>
2023-06-21 17:21:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function checkWeather(city) {
       case 'Clear':
         weather_img.src="clear.png"
         break;
-        case 'Haze':
+        case 'Rain':
             weather_img.src="rain.png"
             break;
             case 'Mist':
